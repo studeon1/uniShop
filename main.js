6d3f479541de2410701688415a29857a6ae75eba
studeon1 <purebo@163.com>
2022-09-24 13:39:18 +0800

    完成首页功能开发

--- main.js
+++ main.js
@@ -1,25 +1,89 @@
 
+// import store from './store'
+// 导入网络请求包
+import { $http } from '@escook/request-miniprogram'
+
+
+uni.$http = $http
+
+//请求的根路径
+$http.baseUrl='http://api-ugo-web.itheima.net'
+// console.log(uni.$http)
+// 请求拦截器
+$http.beforeRequest = function(options){
+	uni.showLoading({
+		title:'数据加载中...'
+	})
+}
+
+// 响应拦截器
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
+
+// 封装弹框的方法
+uni.$showMsg = function(title='数据请求失败！',duration=1500){
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+		
+	})
+}
+
+// // #ifndef VUE3
+// import Vue from 'vue'
+// import App from './App'
+
+// Vue.config.productionTip = false
+
+// App.mpType = 'app'
+
+// const app = new Vue({
+//     ...App
+// })
+// app.$mount()
+// // #endif
+
+// // #ifdef VUE3
+// import { createSSRApp } from 'vue'
+// import App from './App.vue'
+// export function createApp() {
+//   const app = createSSRApp(App)
+//   return {
+//     app
+//   }
+// }
+// // #endif
+
+
+// 非Vue3部分
 // #ifndef VUE3
 import Vue from 'vue'
-import App from './App'
-
 Vue.config.productionTip = false
-
+Vue.prototype.$http = $http
+// Vue.prototype.$store = store
 App.mpType = 'app'
-
 const app = new Vue({
-    ...App
+  // store,
+  ...App
 })
 app.$mount()
 // #endif
 
+
+// Vue3部分
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
-import App from './App.vue'
+import {
+  createSSRApp
+} from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
+  // app.use(store)
+  app.config.globalProperties.$http = $http
   return {
     app
   }
 }
 // #endif
+
